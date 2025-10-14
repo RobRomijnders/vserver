838f8692dd79d8c0f167c29d9eb0d6b180ccac32
Rob Romijnders <romijndersrob@gmail.com>
2025-10-14 16:53:45 +0200

    Update run script.

--- run.sh
+++ run.sh
@@ -1,23 +1,38 @@
 #!/bin/bash
 
 # hostname --ip-address
-MODEL="facebook/opt-125m"
-MODEL_NAME="opt-125m"
+# MODEL="facebook/opt-125m"
+# MODEL_NAME="opt-125m"
+# MAX_MODEL_LEN=1024
 
-MODEL="mistralai/Mistral-7B-v0.3"
-MODEL_NAME="mistralai/Mistral-7B-v0.3"
+# MODEL="gpt2"
+# MODEL_NAME="gpt2"
+# MAX_MODEL_LEN=1024
 
-MODEL="gpt2"
-MODEL_NAME="gpt2"
+export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
+
+
+MODEL="facebook/opt-350m"
+MODEL_NAME="facebook/opt-350m"
+MAX_MODEL_LEN=2048
 
 export VLLM_USE_V1=0
 export CUDA_VISIBLE_DEVICES=0  # Use GPU 0 instead of -1
 
+	# --gpu-memory-utilization 0.8 \
+	# --max-num-batched-tokens 2048 \
+
 python -m vllm.entrypoints.openai.api_server \
-	        --model $MODEL \
-		        --served-model-name $MODEL_NAME \
-			        --port 8000 \
-				        --host 0.0.0.0 \
-					        --trust-remote-code \
-						        --enforce-eager \
-							        --max-model-len 1024
+	--model $MODEL \
+	--served-model-name $MODEL_NAME \
+	--port 8000 \
+	--host 0.0.0.0 \
+	--trust-remote-code \
+	--enforce-eager \
+	--max-model-len $MAX_MODEL_LEN \
+	--max-num-seqs 256 \
+	--max-num-batched-tokens 8192 \
+	--disable-log-requests \
+	--gpu-memory-utilization 0.9 \
+	--tensor-parallel-size 1 \
+	--pipeline-parallel-size 1
